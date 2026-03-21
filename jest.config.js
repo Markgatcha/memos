5b9aeeac3c74cd7893a87f8b30e0e33982e5c255
Mark Gatcha <93056348+Markgatcha@users.noreply.github.com>
2026-03-20 23:23:05 -0400

    fix: update jest.config.js to use NodeNext module for ts-jest ESM

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,18 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  extensionsToTreatAsEsm: [".ts"],
   transform: {
     "^.+\\.tsx?$": [
       "ts-jest",
       {
         useESM: true,
+        tsconfig: {
+          module: "NodeNext",
+          moduleResolution: "NodeNext",
+        },
       },
     ],
   },
+  extensionsToTreatAsEsm: [".ts"],
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.test.ts"],
   moduleNameMapper: {
